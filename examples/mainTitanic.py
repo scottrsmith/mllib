5f192b2ee054f630adec3dc17ecfa60207d73bd5
Scott Smith <scott@scottrobertsmith.com>
2019-03-20 18:00:11 -0700

    Update to current version of MLLib
    Updated to current version

--- examples/mainTitanic.py
+++ examples/mainTitanic.py
@@ -2,67 +2,27 @@
 # -*- coding: utf-8 -*-
 
 
-from project import mlProject
-import pandas as pd
-import numpy as np
-
-from getData import getData
-import exploreData as ed
-from exploreData import exploreData
-from cleanData import cleanData, cleaningRules
-from prepData import prepData
-from trainModels import trainModels
+from mlLib.project import mlProject, loadPredictProject
+import mlLib.exploreData as ed
+import mlLib.trainModels as tm
+from sklearn.exceptions import DataConversionWarning
+import warnings
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
 
-
-"""
-Created on Wed May 16 13:44:03 2018
-
-@author: scottsmith
-"""
-
-
-'''
-
-
-
-enet
---------
-('R^2:', 0.4052451373117357)
-('MAE:', 86298.63725312549)
-('enet', 0.3428746286638919)
-
-lasso
---------
-('R^2:', 0.40888624716724375)
-('MAE:', 85035.54246538795)
-('lasso', 0.3086275085937654)
-
-ridge
---------
-('R^2:', 0.4093396476329719)
-('MAE:', 84978.03564808935)
-('ridge', 0.3166111585985651)
-
-gb
---------
-('R^2:', 0.5410951822821564)
-('MAE:', 70601.60664940192)
-('gb', 0.4869720585739858)
-
-rf
---------
-('R^2:', 0.5722509742910005)
-('MAE:', 67962.75780160858)
-('rf', 0.4815967347888209)
-'''
 
 project = mlProject('Titanic Survival', 'Udacity')
 
-project.setTrainingPreferences (crossValidationSplits=10, parallelJobs=-1, modelType='regression', modelList=['lasso', 'ridge', 'enet', 'rf', 'gb'] ) 
+#modelList=['bagging']
+#modelList=['l1', 'l2', 'rfc', 'decisiontree', 'kneighbors', 'bagging', 'adaboost', 'baggingbase', 'adaboostbase', 'gaussiannb']
+modelList=['bagging']
+#modelList=None  #defauklt is All
+project.setTrainingPreferences (crossValidationSplits=10, parallelJobs=-1, modelType=tm.TRAIN_CLASSIFICATION, modelList=modelList, 
+                                useStandardScaler = True,fbeta=None, defaultHyperparameters=None, hyperparametersLongRun=True)
+
 
 #project.openProject('Predict Home Values2', type='csv', description='Real Estate Project 2', fileName='original_real_estate_data.csv',  hasHeaders = True)
-project.importFile('Survival Data', type='csv', description='Survival Data', fileName='titanic_data.csv',  hasHeaders = True,)
+project.importFile('Survival Data', type='csv', description='Survival Data', fileName='titanic_train.csv',  hasHeaders = True)
 
 
 project.setTarget('Survived')
@@ -73,105 +33,59 @@ project.exploreData()
    
 
 
-print project.explore['Survival Data']
-print project.explore['Survival Data'].allStatsSummary()
+print (project.explore['Survival Data'])
+print (project.explore['Survival Data'].allStatsSummary())
 project.explore['Survival Data'].plotExploreHeatMap()
-
 project.explore['Survival Data'].plotHistogramsAll(10)
-
 project.explore['Survival Data'].plotCorrelations()
 
 
-#project.runCleaningRules()
 
-
-# Add manual Rules
-
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'roof', [['asphalt,shake-shingle','shake-shingle'], 'Shake Shingle'])
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'exterior_walls', [ ['Rock, Stone'], 'Masonry'])
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'exterior_walls', [ ['Concrete','Block'], 'Concrete Block'])
-#project.addManualRuleForDefault(ed.EXPLORE_REMOVE_ITEMS_ABOVE, 'lot_size', 1220551)
-
-# Status
-
-
-
-
-
-#********************************************************
+#########
+# Feature Engineer
 #
-#  
-#  values are sparse classes and prompt to combine
+#project.addManualRuleForDefault(ed.CLEANDATA_REMOVE_ITEMS_EQUAL, 'CustomerID', None)
+#project.addManualRuleForDefault(ed.CLEANDATA_CONVERT_DATATYPE, 'CustomerID', 'int64')
+#project.addManualRuleForDefault(ed.CLEANDATA_NEW_FEATURE, 'Sales', 'Quantity * UnitPrice')
+#project.addManualRuleForDefault(ed.CLEANDATA_REMOVE_ITEMS_EQUAL, 'department', 'temp')
+#project.addManualRuleForDefault(ed.CLEANDATA_REBUCKET, 'department', [['information_technology'], 'IT'])
+#project.addManualRuleForDefault(ed.CLEANDATA_NUMERIC_MARK_MISSING, 'last_evaluation', None)
+#project.addManualRuleForDefault(ed.CLEANDATA_DROP_NA)
+#project.addManualRuleForDefault(ed.CLEANDATA_REMOVE_ITEMS_BELOW, 'property_age',-1)
+#project.addManualRuleForDefault(ed.CLEANDATA_DROP_COLUMN,'tx_year', None)
+
+
+project.initCleaningRules()
+project.addManualRuleForDefault(ed.CLEANDATA_NUMERIC_MARK_MISSING, 'Age', None)
+project.addManualRuleForDefault(ed.CLEANDATA_ZERO_FILL, 'Fare')
+project.addManualRuleForDefault(ed.CLEANDATA_DROP_COLUMN,['Passengerid','Name','Ticket','Cabin'], None)
+project.addManualRuleForDefault(ed.CLEANDATA_DROP_NA)
+
+
+project.cleanAndExploreProject()
 #
-#********************************************************
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'exterior_walls', [['Wood Siding','Wood Shingle'], 'Wood'])
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'exterior_walls', [['Concrete Block', 'Stucco', 'Masonry', 'Asbestos shingle'], 'Other'])
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'roof', [['Composition', 'Wood Shake/ Shingles'], 'Composition Shingle'])
-#project.addManualRuleForDefault(ed.EXPLORE_REBUCKET, 'roof', [['Gravel/Rock', 'Roll Composition', 'Slate', 'Built-up', 'Asbestos', 'Metal'], 'Other'])
-
-#********************************************************
-## Feature Engineer - needs to be user driven
-#********************************************************
-    
-
-#
-#  indicator variable 
-#    
-
-# Create indicator variable for properties with 2 beds and 2 baths
-#df['two_and_two'] = ((df['beds']==2) & (df['baths']==2)).astype(int)
-#project.addManualRuleForDefault(ed.EXPLORE_NEW_INDICATOR_VARIABLE, 'two_and_two', '( beds == 2 ) & ( baths == 2 )')
-
-
-# Create indicator feature for transactions between 2010 and 2013, - resession
-#df['during_recession'] = df['tx_year'].between(2010,2013).astype(int)
-
-#project.addManualRuleForDefault(ed.EXPLORE_NEW_INDICATOR_VARIABLE, 'during_recession', '( tx_year >= 2010 ) & ( tx_year <= 2013 )')
-       
-#
-#  new variables - needs to be user driven
-#
-
-# Create a school score feature that num_schools * median_school
-#df['school_score'] = df['num_schools'] * df['median_school']
-#project.addManualRuleForDefault(ed.EXPLORE_NEW_VARIABLE, 'school_score', 'num_schools * median_school')
-
-# Create a property age feature
-#project.addManualRuleForDefault(ed.EXPLORE_NEW_VARIABLE, 'property_age', 'tx_year - year_built')
-#project.addManualRuleForDefault(ed.EXPLORE_REMOVE_ITEMS_BELOW, 'property_age',-1)
-
-# Drop 'tx_year' and 'year_built' from the dataset
-#df = df.drop(['tx_year','year_built'], axis=1)
-
-#project.addManualRuleForDefault(ed.EXPLORE_DROP_COLUMN,'tx_year', None)
-#project.addManualRuleForDefault(ed.EXPLORE_DROP_COLUMN,'year_built', None)
+project.prepProjectByName('Survival Data')
+project.trainProjectByName('Survival Data')
 
 
 
-
-#project.cleanAndExploreProject()
-#
-#project.prepProjectByName('Property Data')
-#project.trainProjectByName('Property Data')
-#
-#project.exportBestModel('realEstateBestModel.plk')
-#project.exportNamedModel('gb','gbRealEstateModel.plk')
-#project.exportNamedModel('lasso','lassoRealEstateModel.plk')
-#project.exportNamedModel('ridge','ridgeRealEstateModel.plk')
-#project.exportNamedModel('enet','enetRealEstateModel.plk')
-#project.exportNamedModel('rf','rfRealEstateModel.plk')
+print 
+print ('The best is ', project.bestModelName)
+print
+print (project.bestModel)
 
 
-#print 
-#print 'the best is ', project.bestModelName
-#print
-#print project.bestModel
-
-
+project.displayAllScores('Survival Data')
+project.reportResultsOnTrainedModel('Survival Data',project.bestModelName)
 
 
 #
+predict = project.createPredictFromBestModel('Survival Data')
+predict.importPredictFile('Kaggle Data', type='csv', description='Raw Data', fileName='titanic_test.csv',  hasHeaders = True)
+predict.prepPredict()
+ans = predict.runPredict()
+predict.addToPredictFile('Survived',ans)
+predict.removeFromPredictFile(['Pclass','Sex','Age','SibSp','Parch','Fare','Embarked','Age_missing'])
+predict.exportPredictFile('kaggleSubmit.csv')
 
-
-
-# Pick the winner
+#print (ans)
